b7c8e0431e3b3f3702c086469dffa90ca8b24f8a
pq27120 <pq27120@126.com>
2015-07-21 15:17:35 +0800

    斜杠去除

--- git_customer/src/test/java/com/xuan/StringTest.java
+++ git_customer/src/test/java/com/xuan/StringTest.java
@@ -23,7 +23,7 @@ public class StringTest {
 
     @Test
     public void testUrlEncoder(){
-        String html = "<head><title>对象已移动</title></head><body><h1>对象已移动</h1>可以在<a HREF=\"e.asp?e=请在该产品到期前1日内进行积分免费续期！\">此处</a>.</body>";
+        String html = "<head><title>对象已移动</title></head><body><h1>对象已移动</h1>可以在<a HREF=e.asp?e=请在该产品到期前1日内进行积分免费续期！>此处</a>.</body>";
         String regEx_html = "<[^>]"; // 定义HTML标签的正则表达式
         Pattern p_html = Pattern.compile(regEx_html, Pattern.CASE_INSENSITIVE);
         Matcher m_html = p_html.matcher(html);
